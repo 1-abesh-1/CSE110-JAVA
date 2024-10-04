02a3f4a9477e12883db82a087b6d87a964dcfe8f
Sadman Sakib Abesh <sadmansakibabesh@gmail.com>
2024-10-05 00:52:15 +0600

    Add files via upload

---
+++ lecture-3/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args){
+  //lecture of arithmetic operators     
+System.out.println(8 + 9); //additon
+System.out.println(15-9); //substraction
+System.out.println(5 * 5); //multiplication
+System.out.println(9.0/2);//division
+//to get 4.5 we need 9.0. the 9/2 returns 4
+System.out.println(15 % 4);//modulus
+    }
+}

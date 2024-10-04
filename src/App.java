d4b23c3b93fb5d313e98e17d58e51634ecb0fdb4
Sadman Sakib Abesh <sadmansakibabesh@gmail.com>
2024-10-04 16:13:45 +0600

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        byte num = 28; //1 byte	===Stores whole numbers from -128 to 127
+        short num1=7587 ; //2 bytes===Stores whole numbers from -32,768 to 32,767
+        long num2 =5875587; //8 bytes===Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        float num3=6f;//4 bytes===Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+        double num4=4.587;//	8 bytes	Stores fractional numbers. Sufficient for storing 15 decimal digits
+        char ch='A'; //2 bytes	Stores a single character/letter or ASCII values
+        boolean Argument =true;
+
+
+        //these are primitive datatypes
+    }
+}

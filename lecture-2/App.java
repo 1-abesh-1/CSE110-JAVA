2ce425d3e837608b245b554927a3fa57fb8f6527
Sadman Sakib Abesh <sadmansakibabesh@gmail.com>
2024-10-04 20:27:04 +0600

    Add files via upload

---
+++ lecture-2/App.java
@@ -0,0 +1,10 @@
+public class App {//name of the public class
+    public static void main(String[] args){
+       //printing statement
+       System.out.print("hello world");//output: hello world
+       System.out.println("hello world");//output: helloworld
+       double val= 5.7876;
+       System.err.printf("Formatted value =%.2f\n",val);//output: 5.79
+
+    }
+}

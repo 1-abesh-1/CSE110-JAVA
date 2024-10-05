b58d5d9ea2cdd99ed8f3b87e13c322ee29b555bd
Sadman Sakib Abesh <sadmansakibabesh@gmail.com>
2024-10-05 11:22:42 +0600

    Add files via upload

---
+++ lecture-4/relational_operator.java
@@ -0,0 +1,11 @@
+public class relational_operator {
+    public static void main(String[] args){
+  //lecture of relational operators     
+System.out.println(2<1);//less than
+System.out.println(2>1);//greater than
+System.out.println(2 <= 2);//less or equal
+System.out.println(2>=1);//less or greater
+System.out.println(2==3);//equals
+System.out.println(2!=1);//not equal
+    }
+}

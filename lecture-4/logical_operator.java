804f91e5931d74ba885e3845df8f0a25c92f761f
Sadman Sakib Abesh <sadmansakibabesh@gmail.com>
2024-10-05 11:58:59 +0600

    Add files via upload

---
+++ lecture-4/logical_operator.java
@@ -0,0 +1,9 @@
+public class logical_operator {
+    public static void main(String[] args){
+  //lecture of logical operators     
+  System.out.println(true && false);/*Logical and---> && */
+  System.out.println(false || true);/*Logical or---> || */
+  System.out.println(!true);/*Logical Not --->! */
+
+    }
+}

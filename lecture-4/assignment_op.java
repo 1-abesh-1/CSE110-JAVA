c28bd574f0a2ef9e303b379dec097d1588ecc08d
Sadman Sakib Abesh <sadmansakibabesh@gmail.com>
2024-10-05 10:51:22 +0600

    Add files via upload

---
+++ lecture-4/assignment_op.java
@@ -0,0 +1,12 @@
+public class assignment_op {
+    public static void main(String[] args){
+  //lecture of assignment operators     
+int number=56;
+ number += 1;
+//number -=2;
+//number *=5;
+// number /=6;
+ //number %=5;
+System.out.println(number);
+    }
+}

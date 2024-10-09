b7856a4e6a1bb40ddb8ff5154f4d7d96a17584f0
Sadman Sakib Abesh <sadmansakibabesh@gmail.com>
2024-10-10 00:31:28 +0600

    Add files via upload

---
+++ lecture-8/ifelse_lec.java
@@ -0,0 +1,24 @@
+import java.util.Scanner; //importing the scanner
+
+public class ifelse_lec {
+    public static void main(String[] args){
+ Scanner scn=new Scanner(System.in);
+
+ System.out.print("give a number>>>");
+ int num=scn.nextInt();
+
+
+
+ /// if ,else basic. here 
+ if(num > 10){
+   System.out.println(num);
+ }else{
+   System.err.println("the number is less then 10");
+ }
+
+
+
+}
+
+
+    }

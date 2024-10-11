93cf65829bb646df05d41000cc0d7d3fd1eded8d
Sadman Sakib Abesh <sadmansakibabesh@gmail.com>
2024-10-11 23:09:01 +0600

    Add files via upload

---
+++ lecture-9/nested_loop_lec.java
@@ -0,0 +1,38 @@
+import java.util.Scanner; //importing the scanner
+
+public class nested_loop_lec {
+    public static void main(String[] args){
+ //Scanner scn=new Scanner(System.in);
+//int maxVal=0;
+
+
+int outer=1;
+
+while (outer<=2){
+int inner =1;
+while(inner<=3){
+System.out.println(outer+" "+inner);
+inner +=1;
+}
+
+System.err.println("innerloop off");
+    System.out.println("outer");
+    outer +=1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//it's the example of for loop
+}
+
+
+    }

6a6a1356daabc14bf6a49366501f958fd1cc2b2f
Sadman Sakib Abesh <sadmansakibabesh@gmail.com>
2024-10-11 18:56:05 +0600

    Add files via upload

---
+++ lecture-9/for_lec.java
@@ -0,0 +1,34 @@
+import java.util.Scanner; //importing the scanner
+
+public class for_lec {
+    public static void main(String[] args){
+ Scanner scn=new Scanner(System.in);
+//input===========================
+ System.out.print("for how much time?>>>");
+ int num=scn.nextInt();
+
+ System.out.print("give the text>>>");
+ String txt=scn.next();
+//input============================
+
+
+
+
+//for(initialization;condition;steps){}
+for(int i=0;i<num;i++){
+  
+System.out.println(txt);
+
+}
+
+
+
+
+
+
+
+//it's the example of for loop
+}
+
+
+    }

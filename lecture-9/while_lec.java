c9a169a14feeaa52c8db2e60d3998fc3e3f6ce56
Sadman Sakib Abesh <sadmansakibabesh@gmail.com>
2024-10-11 14:02:34 +0600

    Add files via upload

---
+++ lecture-9/while_lec.java
@@ -0,0 +1,30 @@
+import java.util.Scanner; //importing the scanner
+
+public class while_lec {
+    public static void main(String[] args){
+ Scanner scn=new Scanner(System.in);
+//input===========================
+ System.out.print("for how much time?>>>");
+ int num=scn.nextInt();
+
+ System.out.print("give the text>>>");
+ String txt=scn.next();
+//input============================
+
+
+int i=0;
+while(i<num){
+ 
+System.out.println(txt);
+  ++i;
+}
+
+
+
+
+
+//it's the example of while loop
+}
+
+
+    }
